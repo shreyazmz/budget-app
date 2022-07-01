2555f0124dc902e960c17763047b258906040926
Shreya Mukherji <76198856+shreyazmz@users.noreply.github.com>
2022-07-01 13:31:45 +0530

    homepage html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,20 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  {path: '',redirectTo: 'home',pathMatch: 'full'},
+  {path: 'home', loadChildren: ()=>import ('./home/home.module').then(m=>m.HomePageModule)},
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  }
+    loadChildren: () => import('./login/login.module').then(m=>m.LoginPageModule)
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/ServiceWorkerRegistration.module').then(m=> m.RegistrationPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./forgot-password/forgot-password.module')then(m=> m.ForgotPasswordPageModule)
+  },
 ];
 
 @NgModule({
