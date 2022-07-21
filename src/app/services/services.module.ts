e1b8252b10988fbf6b60a5eebf9cbbcbac8b3a87
Shreya Mukherji <76198856+shreyazmz@users.noreply.github.com>
2022-07-21 11:50:17 +0530

    api integrate

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -10,6 +10,26 @@ export class Webapiservice{
   mainturl:string=" Staging2.hpgamechanger.in/api/abc/"
 }
 
+httpHeader = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'
+  ,'apitoken':'eaed1dae8614485ea4ca1ffd4f75b595'
+  ,'DeviceId':'1'
+  ,'AppVersion':'1.2'
+  ,'Access-Control-Allow-Origin':'*'
+  ,'Access-Control-Allow-Methods':'GET,POST,OPTIONS,DELETE,PUT'
+  ,'DeviceToken':'eaed1dae8614485ea4ca1ffd4f75b595' })
+};
+useridauth:string = "itcapi";
+pass:string  = "ITC@12$8)";
+//urlmain:string = 'http://localhost:63145/';
+urlmain:string  = 'http://15.206.108.144/csoapi/';
+httpHeader1 = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'
+  ,'Access-Control-Allow-origin':'true'
+   })
+};
+
+
 
 import { ServicesPageRoutingModule } from './services-routing.module';
 
@@ -24,4 +44,4 @@ import { ServicesPage } from './services.page';
   ],
   declarations: [ServicesPage]
 })
-export class ServicesPageModule {}
+export class ServicesPageModule {} 
