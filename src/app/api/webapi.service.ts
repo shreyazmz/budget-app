319f4ad89a82f0c58d273367ab40d0866186c318
Shreya Mukherji <76198856+shreyazmz@users.noreply.github.com>
2022-07-21 13:44:34 +0530

    api integration

---
+++ src/app/api/webapi.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebapiService {
+
+  constructor(private http: HttpClient) { }
+  urlmain:string  = 'http://Staging2.hpgamechanger.in/api/abc/';
+  httpHeader1 = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'
+    
+    ,'Access-Control-Allow-origin':'true'
+   
+ })
+  };
+
+  Registration(data): Observable<any> {
+    return this.http.post(this.urlmain+'Registration',data, this.httpHeader1)
+      .pipe(
+        catchError(this.handleError('Registration'))
+      );
+  }
+  test(firstname,lastname): Observable<any> {
+    return this.http.get(this.urlmain+'xyz2/'+firstname+'/'+lastname, this.httpHeader1)
+      .pipe(
+        catchError(this.handleError('Registration'))
+      );
+  }
+    private handleError<T>(operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+        console.error(error);
+        console.log(`${operation} failed: ${error.message}`);
+        return of(result as T);
+      }; 
+  
+}
+
+}
