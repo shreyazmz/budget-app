0e6f00bcd0c1fb8384da2200d5bf87fc70d10368
Shreya Mukherji <76198856+shreyazmz@users.noreply.github.com>
2022-07-21 11:32:04 +0530

    api integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WebapiService {
+  //mainturl:string="http://localhost:62095/api/mobile/"
+ mainturl:string="Staging2.hpgamechanger.in/api/abc/"
+ 
+
+
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
