171b75eba03cda5bb30c5b4bcfee0aed4af9a5df
Shreya Mukherji <76198856+shreyazmz@users.noreply.github.com>
2022-07-21 11:43:12 +0530

    api integrate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,3 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-@Injectable({
-  providedIn: 'root'
-})
-
-export class WebapiService {
-  //mainturl:string="http://localhost:62095/api/mobile/"
- mainturl:string="Staging2.hpgamechanger.in/api/abc/"
- 
-
-
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -26,10 +12,6 @@ const routes: Routes = [
    path: 'registration',
    loadChildren: () => import('./registration/Registration.module').then(m=> m.RegistrationPageModule)
 },
-  {
-    path: 'services',
-    loadChildren: () => import('./services/services.module').then( m => m.ServicesPageModule)
-  },
   //{
    // path: 'forgot-password', 
     //loadChildren: () => import('./forgot-password.module')then(m=> m.ForgotPasswordPageModule)
